bcfe95d1dfef89873dd45c2109db1ee9088a9d43
Edward Ou <edward.ou1998@gmail.com>
2021-11-01 18:59:48 +0800

    Login function

--- Server.py
+++ Server.py
@@ -6,13 +6,16 @@ import json
 
 ip_port = ('127.0.0.1', 8080)
 back_log = 5
-buffer_size = 8192
+buffer_size = 4096
 server = socket(AF_INET, SOCK_STREAM)
 CONTINUE = True
 close_msg = 'EXT_Connection to server closed'
 recv_msg = 'MSR_Message received'
 reg_msg_succ = 'RES_Registration success'
 reg_msg_fail = 'REF_Registration failed! Username already exist'
+login_succ = 'LIS_Successfully login as '
+login_fail_usn = 'LIF_Username not found'
+login_fail_psw = 'LIF_Incorrect password'
 
 
 def ser_loop():
@@ -40,6 +43,15 @@ def ser_loop():
                             con.send(reg_msg_succ.encode('utf-8'))
                         else:
                             con.send(reg_msg_fail.encode('utf-8'))
+                    elif header == 'LGI':
+                        usrname, pswd = body.split('_', 1)
+                        status = login(usrname, pswd)
+                        if status == 1:
+                            con.send(login_succ.encode('utf-8'))
+                        elif status == 0:
+                            con.send(login_fail_usn.encode('utf-8'))
+                        else:
+                            con.send(login_fail_psw.encode('utf-8'))
                     # con.send(recv_msg.encode('utf-8'))
                     print('Message: ', body)
                 except Exception as e:
@@ -65,6 +77,21 @@ def register(usrname, pswd):
     print('Registration completed')
     return True
 
+
+def login(username, password):
+    userinfo = {}
+    file = open('userinfo.json', 'r')
+    userinfo = json.load(file)
+    ex_flag = False
+    for key in userinfo:
+        if key == username:
+            ex_flag = True
+            break
+    if not ex_flag:
+        return 0  # username not found
+    if userinfo[username] == password:
+        return 1  # login is successful
+    return -1   # incorrect password
 
 
 def main():
--- client.py
+++ client.py
@@ -1,78 +1,9 @@
 import string
 from socket import *
-from select import select
-import tkinter as tk
-import tkinter.messagebox
-import sys
 
 ip_port = ('127.0.0.1', 8080)
-buffer_size = 8192
+buffer_size = 4096
 p = socket(AF_INET, SOCK_STREAM)
-
-
-class LoginWindow:
-    def __init__(self, master, client):
-        self.master = master
-        self.master.title('Global Connections Login/Registration Window')
-        self.master.geometry('480x300+200+200')
-        self.master.minsize(width=480, height=300)
-        self.master.maxsize(width=480, height=300)
-        self.master.config(bg='black')
-        self.titleLabel = tk.Label(self.master, text="Please Enter Your Information", font=('calibri', 24, 'bold'),
-                                   bg='black', fg='white')
-        self.titleLabel.place(relx=0.05, rely=0.01)
-        self.usrn_label = tk.Label(self.master, text="Username: ", font=('calibri', 16, 'bold'), bg='black', fg='white')
-        self.usrn_label.place(relx=0.05, rely=0.2)
-        self.usrn_entry = tk.Entry(self.master)
-        self.usrn_entry.place(relx=0.32, rely=0.2, width=300)
-        self.pw_label = tk.Label(self.master, text="Password: ", font=('calibri', 16, 'bold'), bg='black', fg='white')
-        self.pw_label.place(relx=0.05, rely=0.4)
-        self.pw_entry = tk.Entry(self.master, show='*')
-        self.pw_entry.place(relx=0.32, rely=0.4, width=300)
-        self.clearBTN = tk.Button(self.master, text='Clear Entry', command=self.cleartext, height=1, width=10,
-                                  font=('calibri', 16, 'bold'))
-        self.clearBTN.place(relx=0.5, rely=0.6)
-        self.loginBTN = tk.Button(self.master, text='Login',
-                                  command=lambda: self.login(self.usrn_entry.get(), self.pw_entry.get(), client),
-                                  height=1, width=10, font=('calibri', 16, 'bold'))
-        self.loginBTN.place(relx=0.2, rely=0.6)
-        self.RegBTN = tk.Button(self.master, text='Register',
-                                command=lambda: self.register(self.usrn_entry.get(), self.pw_entry.get(), client),
-                                height=1, width=10, font=('calibri', 16, 'bold'))
-        self.RegBTN.place(relx=0.2, rely=0.8)
-        self.exitBTN = tk.Button(self.master, text='Exit', command=self.closewindow, height=1, width=10,
-                                 font=('calibri', 16, 'bold'))
-        self.exitBTN.place(relx=0.5, rely=0.8)
-
-    def login(self, username, pswd, client):
-        try:
-            client.connect(('127.0.0.1', 8080))
-        except timeout:
-            print('Socket error: timeout')
-
-    def register(self, username, pswd, p):
-        try:
-            p.connect(('127.0.0.1', 8080))
-        except timeout:
-            print('Socket error: timeout')
-
-    def cleartext(self):
-        self.usrn_entry.delete(0, tk.END)
-        self.pw_entry.delete(0, tk.END)
-
-    def closewindow(self):
-        terminate = tkinter.messagebox.askyesno('Confirmation', 'Do you want to terminate the program?')
-        if terminate > 0:
-            self.master.destroy()
-            sys.exit()
-        else:
-            pass
-
-
-def main():
-    root = tk.Tk()
-    app = LoginWindow(root, p)
-    root.mainloop()
 
 
 def getheader(x, msg):
